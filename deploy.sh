6aa6e41b055d0716220562b0eae0ac10824b4d87
Sean Caron <scaron@umich.edu>
2022-01-25 17:18:05 -0500

    Update deployment script

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,6 @@ if [ "$1" == "core" ]; then
     mkdir -p /usr/lib/cgi-bin/usermanager
     cp administrator_dashboard.py /usr/lib/cgi-bin/usermanager
     cp approver_dashboard.py /usr/lib/cgi-bin/usermanager
-    cp scan_and_create.py /usr/lib/cgi-bin/usermanager
     cp usermanager_form_handler.py /usr/lib/cgi-bin/usermanager
     cp .htaccess /usr/lib/cgi-bin/usermanager
 
@@ -29,24 +28,31 @@ if [ "$1" == "core" ]; then
 
     cp scan_and_create.py /opt/usermanager/bin
     cp scan_and_disable.py /opt/usermanager/bin
+    cp scan_and_unlock_trained.py /opt/usermanager/bin
     cp usermanager_importer.py /opt/usermanager/bin
     cp usermanager_home_agent.py /opt/usermanager/bin
 
+    #
+    # Deploy configuration and templates
+    #
+    
     if [ ! -f /opt/usermanager/etc/usermanager.ini ]; then
         cp usermanager.ini /opt/usermanager/etc
         chmod 644 /opt/usermanager/etc/usermanager.ini
     fi
 
+    cp templates/*.tpl /opt/usermanager/etc
+    
     #
     # Deploy Web form
     #
 
     rm -rf /var/www/usermanager
     mkdir -p /var/www/usermanager
-    cp index.html /var/www/usermanager
-    cp style.css /var/www/usermanager
-    cp logo.png /var/www/usermanager
-    cp .htaccess /var/www/usermanager
+    cp webform/index.html /var/www/usermanager
+    cp webform/style.css /var/www/usermanager
+    cp webform/logo.png /var/www/usermanager
+    cp webform/dot.htaccess /var/www/usermanager/.htaccess
 
 elif [ "$1" == "homeagent" ]; then
     rm -rf /opt/usermanager/bin
