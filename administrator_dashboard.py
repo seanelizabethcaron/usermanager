0f4b1be1cf837bb53386532f4960ebbc7218ea7a
Sean Caron <scaron@umich.edu>
2022-08-03 16:35:29 -0400

    More Python 3 fixes

--- administrator_dashboard.py
+++ administrator_dashboard.py
@@ -254,10 +254,10 @@ if fm_selectuser:
             user_password = results['userPassword'][0]
 
             # GoSA marks accounts as being locked by putting an exclamation point between the hash type and the hash itself
-            unlocked_pwd = user_password.replace("}!", "}")
+            unlocked_pwd = user_password.decode().replace("}!", "}")
 
             # Write back the updated password to LDAP to unlock the account
-            modl = [(ldap.MOD_REPLACE, 'userPassword', [unlocked_pwd])]
+            modl = [(ldap.MOD_REPLACE, 'userPassword', [unlocked_pwd.encode()])]
             l.modify_s(dn, modl)
 
             # Unbind from the LDAP directory
