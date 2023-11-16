d3319be0749762050a22f68943f63012d24c6018
Sean Caron <scaron@umich.edu>
2023-11-16 16:38:19 -0500

    Fix bug involving wrong database handle when processing Samba users

--- scan_and_unlock_trained.py
+++ scan_and_unlock_trained.py
@@ -169,16 +169,16 @@ for username in report:
         local_curs.execute(query)
 
         # Unlock the samba account if applicable
-        
-        curs = db.cursor()
+
+        curs = local_db.cursor()
         query = 'SELECT * from samba where uniqname = \'' + uniqname + '\';'
         curs.execute(query)
         result = curs.fetchone()
-    
+
         # If the user shows up in the samba table then we know that a Samba account exists for them
         if result != None:
             # Get the home directory host for the account
-            curs = db.cursor()
+            curs = local_db.cursor()
             query = 'SELECT * FROM homes WHERE serialnum = ' + str(serialnum) + ';'
             if debug:
                 debug_time = time.strftime("%A %b %d %H:%M:%S %Z", time.localtime())
@@ -188,13 +188,13 @@ for username in report:
             home_host = report[1]
 
             # Create a smbpasswd_workqueue entry to reenable the Samba account of the user
-            curs = db.cursor()
+            curs = local_db.cursor()
             query = 'INSERT INTO smbpasswd_workqueue (host, uniqname, action, ready) VALUES (\'' + home_host + '\',\'' + uniqname + '\',\'e\',1);'
             if debug:
                 debug_time = time.strftime("%A %b %d %H:%M:%S %Z", time.localtime())
                 debuglog.write(debug_time + ': SQL query string is ' + query + '\n')
             curs.execute(query)
-            db.commit()
+            local_db.commit()
 
         if audit:
             audit_time = time.strftime("%A %b %d %H:%M:%S %Z", time.localtime())
@@ -293,4 +293,3 @@ for username in report:
             local_curs.execute(query)
 
 local_db.commit()
-
